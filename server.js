d764176f1fd207c3c1465adc06d753b327a035bf
다영 <86220363+imdayoung@users.noreply.github.com>
2022-06-01 21:16:59 +0900

    검색 결과 send

--- server.js
+++ server.js
@@ -17,6 +17,26 @@ app.listen(port, function(req, res) {
 	console.log('server run: '+port);
 })
 
-app.post('/', function(req, res){
-	
+app.get("/", function(req, res) {
+	var sqlTest = "SELECT user_nickname FROM `USER` WHERE user_id='dlekdud0102'";
+	db.query(sqlTest, (err, result) => {
+		console.log(result);
+		res.send(result);
+	})
+});
+
+// 검색
+app.post('/search', function(req, res) {
+	const target = req.body.target;
+	var sqlSearch = "SELECT product_title, product_price, product_img FROM `PRODUCT` WHERE product_title='%?%'";
+	db.query(sqlSearch, target, (err, result) => {
+		if(err) {
+			console.log("search error");
+			res.send({err: err});
+		}
+		if (result) {
+			console.log(result);
+			res.send(result);
+		}
+	})
 })
